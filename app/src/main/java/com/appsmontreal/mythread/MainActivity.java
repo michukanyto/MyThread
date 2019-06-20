a9ae3b3955317012a2271b1334c6c1d15e4890c4
Marlon <escobar.marlon@gmail.com>
2019-06-20 19:50:40 -0400

    Handler implementation

--- app/src/main/java/com/appsmontreal/mythread/MainActivity.java
+++ app/src/main/java/com/appsmontreal/mythread/MainActivity.java
@@ -3,17 +3,22 @@ package com.appsmontreal.mythread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
+    private Button startButton;
+    private Handler myHandler = new Handler();//take the thread where it is instantiated in this case UI thread
+                                              //so I could use wherever I want just usin method post(new Runnable)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.startButton);
     }
 
     public void start(View view){
@@ -62,6 +67,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             for (int i = 0; i < seconds; i++){
+                if (i == 4 ){
+                    myHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            startButton.setText("hello");
+                        }
+                    });
+                }
                 Log.d(TAG, "start: " + i);
 
                 try {

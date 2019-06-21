6851b129bd99407fe4cddd51abd9b5229d8c21be
Marlon <escobar.marlon@gmail.com>
2019-06-20 20:29:40 -0400

    Stop button implemented

--- app/src/main/java/com/appsmontreal/mythread/MainActivity.java
+++ app/src/main/java/com/appsmontreal/mythread/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private Button startButton;
+    private volatile Boolean stopThread = false;
     private Handler myHandler = new Handler();//take the thread where it is instantiated in this case UI thread
                                               //so I could use wherever I want just usin method post(new Runnable)
     @Override
@@ -22,17 +23,45 @@ public class MainActivity extends AppCompatActivity {
         startButton = findViewById(R.id.startButton);
     }
 
-    public void start(View view){
+    public void start(View view) {
         //////////////Solution 1//////////////////
 //        ExampleThread exampleThread = new ExampleThread(5);
 //        exampleThread.start();
 
         //////////////Solution 2//////////////////
+        stopThread = false;
         ExampleRunnable exampleRunnable = new ExampleRunnable(6);
         new Thread(exampleRunnable).start();
+
+
+        ///////////Solution 3//////////////Anonymous Class Thread
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < 6; i++) {
+//                    if (i == 2) {
+//                        myHandler.post(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                startButton.setText("hello");
+//                            }
+//                        });
+//                    }
+//                    Log.d(TAG, "start: " + i);
+//
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//
+//        }).start();
     }
 
     public void stop(View view) {
+        stopThread = true;
     }
 
     //////////////Solution 1//////////////////
@@ -68,15 +97,18 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             for (int i = 0; i < seconds; i++){
+                if(stopThread){
+                    return;
+                }
                 if (i == 2 ){
-                    myHandler.post(new Runnable() {
+                    myHandler.post(new Runnable() {//It's simpler do it like that than  //***
                         @Override
                         public void run() {
                             startButton.setText("hello");
                         }
                     });
                 }
-                else if(i == 5){//***
+                else if(i == 5){//*** Another way scroll down to see explanation
                     Handler threadHandler = new Handler(Looper.getMainLooper());
                     threadHandler.post(new Runnable() {
                         @Override
